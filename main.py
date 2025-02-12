11ed8825f449664a936a19a99f09acbe2e87a215
vanget <vanget@msoe.edu>
2025-02-12 16:00:44 -0600

    #1 Added player along with movement
    #2 Added file clock to max out FPS

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import pygame
 from settings import *
-from map import *
+from map import Map
+from player import Player
 
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
 RUN = True
 
 map = Map()
+player = Player()
+
+clock = pygame.time.Clock()
 
 while RUN:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
+    player.update()
 
     screen.fill((0,0,0))
 
     map.render(screen)
+    player.render(screen)
 
     pygame.display.update()
